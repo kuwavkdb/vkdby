2715fbd4cb2cc076a2e1141c77159aed44eb44a6
kuwa <kuwa@gmail.com>
2026-01-25 16:33:33 +0900

    feat: improve Person edit page with links editing and layout updates
    - Add Official Links editing section to Person edit page
    - Change layout to 2-column grid (form on left, links on right)
    - Make key and old_key read-only in form (display only)
    - Add _links_form.html.erb partial for link management
    - Update controller to permit links_attributes
    - Always build empty link field for adding new links

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -16,6 +16,7 @@ class Admin::PeopleController < Admin::BaseController
 
   def edit
     @person_logs = @person.person_logs.order(:log_date)
+    @person.links.build # Always add an empty link field for new entries
   end
 
   def create
@@ -48,6 +49,6 @@ class Admin::PeopleController < Admin::BaseController
   end
 
   def person_params
-    params.require(:person).permit(:name, :name_kana, :key, :birthday, :birth_year, :blood, :hometown, :status, :old_key, parts: [])
+    params.require(:person).permit(:name, :name_kana, :birthday, :birth_year, :blood, :hometown, :status, parts: [], links_attributes: [:id, :text, :url, :active, :_destroy])
   end
 end
