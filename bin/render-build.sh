36fbc8c159d11615999afe46bef759358dbc4156
kuwa <kuwa@gmail.com>
2026-02-01 09:41:46 +0900

    chore: Add conditional data import to render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,10 @@ bundle install
 bundle exec rails tailwindcss:build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+if [ "$IMPORT_DATA" = "true" ]; then
+  echo "Running data import..."
+  bundle exec rails db:migrate # Ensure DB is migrated before import
+  bundle exec rails import:units
+  bundle exec rails import:people
+fi
