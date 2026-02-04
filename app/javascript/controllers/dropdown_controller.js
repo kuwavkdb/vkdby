f3a6d0e9d1c985618705180bbe60e30c0fc5f843
kuwa <kuwa@gmail.com>
2026-02-05 00:21:35 +0900

    Fix dropdown controller to handle transitions correctly

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -26,7 +26,8 @@ export default class extends Controller {
 
     show() {
         this.menuTarget.classList.remove("hidden")
-        this.menuTarget.classList.add("enter-active") // For transitions if needed
+        this.enter()
+
         // Rotate icon if exists
         const icon = this.buttonTarget.querySelector("svg")
         if (icon) icon.classList.add("rotate-180")
@@ -36,10 +37,68 @@ export default class extends Controller {
         if (event && (this.element.contains(event.target))) {
             return
         }
-        this.openValue = false
-        this.menuTarget.classList.add("hidden")
+
+        // If called from event (click outside), update state which triggers openValueChanged -> hide()
+        if (this.openValue) {
+            this.openValue = false
+            return
+        }
+
+        this.leave()
 
         const icon = this.buttonTarget.querySelector("svg")
         if (icon) icon.classList.remove("rotate-180")
+    }
+
+    enter() {
+        const element = this.menuTarget
+        const enterClasses = this.getClasses("transitionEnter")
+        const enterStartClasses = this.getClasses("transitionEnterStart")
+        const enterEndClasses = this.getClasses("transitionEnterEnd")
+
+        element.classList.add(...enterClasses)
+        element.classList.add(...enterStartClasses)
+        element.classList.remove(...enterEndClasses)
+
+        requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+                element.classList.remove(...enterStartClasses)
+                element.classList.add(...enterEndClasses)
+
+                element.addEventListener("transitionend", () => {
+                    element.classList.remove(...enterClasses)
+                }, { once: true })
+            })
+        })
+    }
+
+    leave() {
+        const element = this.menuTarget
+        const leaveClasses = this.getClasses("transitionLeave")
+        const leaveStartClasses = this.getClasses("transitionLeaveStart")
+        const leaveEndClasses = this.getClasses("transitionLeaveEnd")
+
+        element.classList.add(...leaveClasses)
+        element.classList.add(...leaveStartClasses)
+        element.classList.remove(...leaveEndClasses)
+
+        requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+                element.classList.remove(...leaveStartClasses)
+                element.classList.add(...leaveEndClasses)
+
+                element.addEventListener("transitionend", () => {
+                    if (!this.openValue) {
+                        element.classList.add("hidden")
+                    }
+                    element.classList.remove(...leaveClasses)
+                    element.classList.remove(...leaveEndClasses)
+                }, { once: true })
+            })
+        })
+    }
+
+    getClasses(name) {
+        return (this.menuTarget.dataset[name] || "").split(" ").filter(c => c.length > 0)
     }
 }
