036b7a28515e12fb517416715f923294a75bd82d
kuwa <kuwa@gmail.com>
2026-02-05 22:50:40 +0900

    feat: ItemCardComponent を作成し、Lookbook に対応
    - 1商品の表示枠を ViewComponent 化
    - with_collection を使用してコレクションレンダリングに対応
    - 画像を左側、情報を右側に配置（160x160）
    - Lookbook プレビューを追加（4つのシナリオ）

---
+++ test/components/previews/item_card_component_preview.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class ItemCardComponentPreview < ViewComponent::Preview
+  layout 'component_preview'
+
+  def with_image
+    item = Item.new(
+      title: 'lynch. 20TH ANNIVERSARY XX FINAL ACT 「ALL THIS WE\'LL GIVE YOU」25.12.28 TOKYO GARDEN THEATER[数量限定盤] - lynch. [Blu-ray]',
+      release_date: Date.new(2026, 4, 22),
+      image_url: 'https://m.media-amazon.com/images/I/51Z1Z1Z1Z1L._SL160_.jpg',
+      link_url: 'https://www.amazon.co.jp/exec/obidos/ASIN/B0EXAMPLE/vkdb-22/',
+      artists: [
+        { 'name' => 'lynch.', 'old_key' => '%A5%EA%A5%F3%A5%C1' }
+      ]
+    )
+
+    render(ItemCardComponent.new(item_card: item))
+  end
+
+  def without_image
+    item = Item.new(
+      title: '【特典付】DIR EN GREY MORTAL DOWNER 【完全生産限定盤】(2CD+Blu-ray)【早期予約特典:オリジナルトレーディングカード+特典:ポストカード】',
+      release_date: Date.new(2026, 4, 8),
+      image_url: nil,
+      link_url: 'https://www.amazon.co.jp/exec/obidos/ASIN/B0EXAMPLE2/vkdb-22/',
+      artists: [
+        { 'name' => 'DIR EN GREY', 'old_key' => '%A5%C7%A5%A3%A5%EB%A5%A2%A5%F3%A5%B0%A5%EC%A5%A4' }
+      ]
+    )
+
+    render(ItemCardComponent.new(item_card: item))
+  end
+
+  def multiple_artists
+    item = Item.new(
+      title: 'V-ROCK FESTIVAL 2026 COMPILATION ALBUM',
+      release_date: Date.new(2026, 5, 15),
+      image_url: 'https://m.media-amazon.com/images/I/51EXAMPLE._SL160_.jpg',
+      link_url: 'https://www.amazon.co.jp/exec/obidos/ASIN/B0EXAMPLE3/vkdb-22/',
+      artists: [
+        { 'name' => 'lynch.', 'old_key' => '%A5%EA%A5%F3%A5%C1' },
+        { 'name' => 'DIR EN GREY', 'old_key' => '%A5%C7%A5%A3%A5%EB%A5%A2%A5%F3%A5%B0%A5%EC%A5%A4' },
+        { 'name' => 'MUCC', 'old_key' => '%A5%E0%A5%C3%A5%AF' }
+      ]
+    )
+
+    render(ItemCardComponent.new(item_card: item))
+  end
+
+  def artist_without_old_key
+    item = Item.new(
+      title: 'NEW ARTIST DEBUT ALBUM',
+      release_date: Date.new(2026, 6, 1),
+      image_url: 'https://m.media-amazon.com/images/I/51NEWARTIST._SL160_.jpg',
+      link_url: 'https://www.amazon.co.jp/exec/obidos/ASIN/B0EXAMPLE4/vkdb-22/',
+      artists: [
+        { 'name' => 'New Artist' }
+      ]
+    )
+
+    render(ItemCardComponent.new(item_card: item))
+  end
+end
