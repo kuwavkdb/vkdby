077c60e1104f656a05dc5ce47f5527dbc2011f7a
kuwa <kuwa@gmail.com>
2026-02-07 02:06:14 +0900

    fix: よみがな検索時に入力をカタカナに変換して検索するように修正

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,7 +3,7 @@
 class UnitsController < ApplicationController
   def index
     scope = Unit.all.order(updated_at: :desc)
-    scope = scope.where('name ILIKE :q OR name_kana ILIKE :q OR name_log::text ILIKE :q', q: "%#{params[:q]}%") if params[:q].present?
+    scope = scope.where('name ILIKE :q OR name_kana ILIKE :q_kana OR name_log::text ILIKE :q', q: "%#{params[:q]}%", q_kana: "%#{params[:q].tr('ぁ-ん', 'ァ-ン')}%") if params[:q].present?
     @pagy, @units = pagy(scope, limit: 60)
   end
 
