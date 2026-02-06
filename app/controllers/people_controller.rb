ba664c4f4c7eea08bc931f3bde9be43eeb6c5667
kuwa <kuwa@gmail.com>
2026-02-07 02:10:43 +0900

    style: ルボコップによる自動修正

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,10 @@
 class PeopleController < ApplicationController
   def index
     scope = Person.where.not(key: [nil, '']).order(updated_at: :desc)
-    scope = scope.where('name ILIKE :q OR name_kana ILIKE :q_kana OR name_log::text ILIKE :q', q: "%#{params[:q]}%", q_kana: "%#{params[:q].tr('ぁ-ん', 'ァ-ン')}%") if params[:q].present?
+    if params[:q].present?
+      scope = scope.where('name ILIKE :q OR name_kana ILIKE :q_kana OR name_log::text ILIKE :q', q: "%#{params[:q]}%",
+                                                                                                 q_kana: "%#{params[:q].tr('ぁ-ん', 'ァ-ン')}%")
+    end
     @pagy, @people = pagy(scope, limit: 60)
   end
 
--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,7 +3,10 @@
 class UnitsController < ApplicationController
   def index
     scope = Unit.all.order(updated_at: :desc)
-    scope = scope.where('name ILIKE :q OR name_kana ILIKE :q_kana OR name_log::text ILIKE :q', q: "%#{params[:q]}%", q_kana: "%#{params[:q].tr('ぁ-ん', 'ァ-ン')}%") if params[:q].present?
+    if params[:q].present?
+      scope = scope.where('name ILIKE :q OR name_kana ILIKE :q_kana OR name_log::text ILIKE :q', q: "%#{params[:q]}%",
+                                                                                                 q_kana: "%#{params[:q].tr('ぁ-ん', 'ァ-ン')}%")
+    end
     @pagy, @units = pagy(scope, limit: 60)
   end
 
