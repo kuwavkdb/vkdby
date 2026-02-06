1bfb9927579cfd8ed2e6345a989728d0f120a260
kuwa <kuwa@gmail.com>
2026-02-07 02:35:55 +0900

    feat: Add tag filtering to units index
    Implements filtering by Japanese syllabary (50-on) tags (index_group_id: 1). Adds filter UI to the units list page and sorts results by name_kana when filtered.

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -2,8 +2,15 @@
 
 class UnitsController < ApplicationController
   def index
+    @tag_indices = TagIndex.where(index_group_id: 1).ordered
+
     scope = Unit.all.order(updated_at: :desc)
     scope = scope.where('name ILIKE :q OR name_kana ILIKE :q OR name_log::text ILIKE :q', q: "%#{params[:q]}%") if params[:q].present?
+    if params[:tag_id].present?
+      scope = scope.joins(:tag_indices).where(tag_indices: { id: params[:tag_id] })
+      scope = scope.reorder(name_kana: :asc)
+    end
+
     @pagy, @units = pagy(scope, limit: 60)
   end
 
